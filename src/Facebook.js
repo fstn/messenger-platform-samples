cf6b37ea367dcddff5d08246c1ecccc4f351f7d9
stephen zambaux <stephen.zambaux@gmail.com>
2016-07-21 21:52:08 +0200

    Refacto du bot

--- src/Facebook.js
+++ src/Facebook.js
@@ -5,31 +5,28 @@ const
     crypto = require('crypto'),
     request = require('request');
 
+// App Secret can be retrieved from the App Dashboard
+const APP_APIAP = (process.env.MESSENGER_APP_SECRET) ?
+    process.env.MESSENGER_APP_SECRET :
+    config.get('apiaiKey');
+
+
+// App Secret can be retrieved from the App Dashboard
+const APP_SECRET = (process.env.MESSENGER_APP_SECRET) ?
+    process.env.MESSENGER_APP_SECRET :
+    config.get('appSecret');
+
+// Arbitrary value used to validate a webhook
+const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
+    (process.env.MESSENGER_VALIDATION_TOKEN) :
+    config.get('validationToken');
+
+// Generate a page access token for your page from the App Dashboard
+const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
+    (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
+    config.get('pageAccessToken');
 
 function Facebook(){
-
-    // App Secret can be retrieved from the App Dashboard
-        const APP_APIAP = (process.env.MESSENGER_APP_SECRET) ?
-            process.env.MESSENGER_APP_SECRET :
-            config.get('apiaiKey');
-
-
-    // App Secret can be retrieved from the App Dashboard
-        const APP_SECRET = (process.env.MESSENGER_APP_SECRET) ?
-            process.env.MESSENGER_APP_SECRET :
-            config.get('appSecret');
-
-    // Arbitrary value used to validate a webhook
-        const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
-            (process.env.MESSENGER_VALIDATION_TOKEN) :
-            config.get('validationToken');
-
-    // Generate a page access token for your page from the App Dashboard
-        const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
-            (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
-            config.get('pageAccessToken');
-
-
     if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN)) {
         console.error("Missing config values");
         process.exit(1);
