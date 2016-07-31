e87d089660467a2cf45c74bd14578be450e495f6
stephen zambaux <stephen.zambaux@gmail.com>
2016-07-31 17:57:06 +0200

    Correction du double ISBN

--- app.js
+++ app.js
@@ -35,17 +35,16 @@ var app = express();
  */
 var bookService = new BookService(app);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.json({data: [1,2,3,4]})
+});
 app.set('port', process.env.PORT || 5000);
 app.use(bodyParser.json({verify: facebook.verifyRequestSignature}));
 app.use('/static', express.static('book'));
 
 
-app.all('/*', function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header("Access-Control-Allow-Headers", "X-Requested-With,     Content-Type");
-  next();
-});
 
 
 /*
