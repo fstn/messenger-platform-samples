3e1cd89825f0fcb3854ebc8f4d4c73baa556a682
stephen zambaux <stephen.zambaux@gmail.com>
2016-07-31 17:53:05 +0200

    Correction du double ISBN

--- app.js
+++ app.js
@@ -39,11 +39,14 @@ app.set('port', process.env.PORT || 5000);
 app.use(bodyParser.json({verify: facebook.verifyRequestSignature}));
 app.use('/static', express.static('book'));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+app.all('/*', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header("Access-Control-Allow-Headers", "X-Requested-With,     Content-Type");
   next();
 });
+
 
 /*
  * Use your own validation token. Check that the token used in the Webhook 
